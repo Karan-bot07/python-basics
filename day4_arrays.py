adb41893d07a07f112ae2f0cbec1d47febe1fd0e
Karan Pal <karanpal0010@gmail.com>
2026-01-08 00:55:14 +0530

    Add files via upload

---
+++ day4_arrays.py
@@ -0,0 +1,20 @@
+# import array as arr
+# Find largest element in array
+# a = [1,0,9,4,2,7,10,3]
+# a.sort(reverse = True)
+# print(a[0])
+
+# # Find sum of array elements
+# a = arr.array('i',[5,1,6])
+# sum = 0
+# for i in a:
+#     sum+=i
+# print(sum)
+
+# Count even numbers in array
+# a = arr.array('i',[1,2,3,4,5,6])
+# count = 0
+# for i in a:
+#     if i%2==0:
+#         count+=1
+# print(count)
