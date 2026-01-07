11d28ab9f3f8335a8e422d28cc05f714cd71d286
Karan <karanpal0010@gmail.com>
2026-01-07 20:51:29 +0530

    Day 4 Python practice problems

---
+++ day4_python_practice.py
@@ -0,0 +1,95 @@
+# Print numbers from 1 to 20
+# for i in range(1,21):
+#     print(i)
+
+# Print all even numbers between 1 and 50
+# for i in range(2,51,2):
+#     print(i)
+
+# Sum of first n natural numbers (input from user)
+# n = int(input("Enter a Number:\n"))
+# sum = 0
+# for i in range(1,n+1):
+#     sum+=i
+# print(sum)
+
+# Reverse a number
+# n = int(input("Enter a Number:\n"))
+# a = 10 
+# rev = 0
+# b = 0
+# while n!=0:
+#     ans = n%10
+#     rev = int(rev*10 + ans)
+#     n = n//a
+# print("Reverse of your input: ",rev)
+
+# Count digits in a number
+# n = int(input("Enter a number:\n"))
+# count = 0
+# a = 10
+# while n!=0:
+#     count+=1
+#     n//=a 
+# print(count)
+
+# Check if a number is palindrome
+# n = int(input("Enter a Number:\n"))
+# m = n
+# a = 10 
+# rev = 0
+# b = 1
+# while n!=0:
+#     ans = n%10
+#     rev = int(rev*10 + ans)
+#     n = n//a 
+#     b = rev*1
+# print("Reverse of your number is: ",rev)
+# if m == b:
+#     print("Its a Palindrome")
+# else:
+#     print("Not a Palindrome")
+
+# Factorial using loop
+# n = int(input("Enter a number whose factorial you want:\n"))
+# a = 1
+# for i in range(1,n+1):
+#     a = a*i
+# print(a)
+
+# Fibonacci series (first n terms)
+# n = int(input("Enter a number:\n"))
+# a = 0
+# b = 1
+# for i in range(1,n+1):
+#     sum  = a + b
+#     print(a," ",b," ",sum," ")
+#     a = b 
+#     b = sum
+
+# Find maximum of 3 numbers
+# n1=int(input("enter a number1:\n"))
+# n2=int(input("enter a number2:\n"))
+# n3=int(input("enter a number3:\n"))
+# if n1>n2 and n1>n3:
+#     print(n1)
+# if n2>n1 and n2>n3:
+#     print(n2)
+# if n3>n1 and n3>n2:
+#     print(n3)
+
+# Count vowels in a string
+a = str(input("enter a string:\n"))
+count = 0
+for i in a:
+    if i == 'a':
+        count+=1
+    if i == 'e':
+        count+=1
+    if i == 'i':
+        count+=1
+    if i == 'o':
+        count+=1
+    if i == 'u':
+        count+=1
+print(count)
